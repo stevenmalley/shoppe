797d292ba33b12ad8280bb3fb8ffb368325485d4
stevenmalley <steven.malley@gmail.com>
2023-01-10 15:29:01 +0000

    book images changed

---
+++ sql/db.sql
@@ -0,0 +1,37 @@
+CREATE TABLE customer (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  username TEXT,
+  email TEXT,
+  password_hash TEXT,
+  google_account boolean
+);
+
+CREATE TABLE product (
+  id INTEGER UNIQUE,
+  name TEXT,
+  description TEXT,
+  quantity INTEGER,
+  price money,
+  author TEXT,
+  image TEXT
+);
+
+CREATE TABLE purchase (
+  id INTEGER UNIQUE,
+  customer_id INTEGER REFERENCES customer(id),
+  datetime DATE
+);
+
+CREATE TABLE sale (
+  id INTEGER,
+  quantity INTEGER,
+  purchase_id INTEGER REFERENCES purchase(id),
+  product_id INTEGER REFERENCES product(id)
+);
+
+CREATE TABLE cart (
+  customer_id INTEGER REFERENCES customer(id),
+  product_id INTEGER REFERENCES product(id),
+  quantity INTEGER
+);
---
+++ sql/products.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id,name,description,quantity,author,price,image) VALUES
+  (1,'War and Peace','Russian aristocratic drama in the Napoleonic era.',6,'Leo Tolstoy',8.99,'war-and-peace'),
+  (2,'Hitchhikers Guide to the Galaxy','Galactic adventures of Ford Prefect and Arthur Dent',10,'Douglas Adams',6.99,'hhgttg'),
+  (3,'The Conquest of Gaul','Julius Caesar''s account of the invasion of Gaul.',11,'Julius Caesar',10.99,'conquest-of-gaul');
+   
