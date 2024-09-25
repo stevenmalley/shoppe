ac74826dbe32c48e464c0365b489579b2a0e163f
stevenmalley <steven.malley@gmail.com>
2024-09-25 17:02:10 +0100

    product tile styles changed

--- shoppe-client/src/features/ProductTile.js
+++ shoppe-client/src/features/ProductTile.js
@@ -5,8 +5,16 @@ import serverPath from '../serverPath';
 
 export default function ProductTile(props) {
 
+  function handleMouseEnter(e) {
+    e.currentTarget.classList.add("focusedTile");
+  }
+
+  function handleMouseLeave(e) {
+    e.currentTarget.classList.remove("focusedTile");
+  }
+
     return (
-        <Link to={`/product/${props.product.id}`} className="productTile">
+        <Link to={`/product/${props.product.id}`} className="productTile" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div>
               <div className="productName">{props.product.name}</div>
               <div className="productDesc">{props.product.description}</div>
